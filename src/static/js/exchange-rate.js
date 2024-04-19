898231e2f7e2c37568e85cd2ea520562c3fd989c
webdigga <webdigga42@gmail.com>
2024-04-19 15:29:49 +0100

    Finishing off currency exchange banner

--- src/static/js/exchange-rate.js
+++ src/static/js/exchange-rate.js
@@ -1,13 +1,14 @@
-const fetchRes = fetch("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ySMNdhAdGoWB8KaSgx1eMmvrrPafLNJ39HpzxObZ&currencies=EUR%2CUSD&base_currency=GBP");
+const fetchRes = fetch("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ySMNdhAdGoWB8KaSgx1eMmvrrPafLNJ39HpzxObZ&currencies=EUR%2CUSD%2CCAD%2CAUD%2CNZD&base_currency=GBP");
 
 fetchRes.then(res =>
 	res.json()).then(rates => {
-
 		document.getElementById('eur-exchange-value').textContent = rates.data.EUR.toFixed(5);
 		document.getElementById('usd-exchange-value').textContent = rates.data.USD.toFixed(5);
+		document.getElementById('aud-exchange-value').textContent = rates.data.AUD.toFixed(5);
+		document.getElementById('nzd-exchange-value').textContent = rates.data.NZD.toFixed(5);
+		document.getElementById('cad-exchange-value').textContent = rates.data.CAD.toFixed(5);
 
-		const currencyContainer = document.getElementById('currencyContainer');
-		currencyContainer.style.visibility = 'visible';
+		document.getElementById('currencyContainer').style.visibility = 'visible';
 	}).catch((e) => {
 		console.log(e);
 	});
